0cc485e5e8a2dea5c2ae1abc76c90284b132cab1
Suraj Sukumaran <17068263+vssuraj@users.noreply.github.com>
2024-09-25 09:42:02 -0500

    Initial commit

---
+++ examples/compliance_service.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Mock database of country information
+country_info = {
+    "US": {"name": "United States", "currency": "USD", "compliance_rules": ["Rule1", "Rule2"]},
+    "CA": {"name": "Canada", "currency": "CAD", "compliance_rules": ["Rule3", "Rule4"]},
+    "GB": {"name": "United Kingdom", "currency": "GBP", "compliance_rules": ["Rule5", "Rule6"]}
+}
+
+@app.route('/country/<code>', methods=['GET'])
+def get_country_info(code):
+    info = country_info.get(code.upper())
+    if info:
+        return jsonify(info)
+    else:
+        return jsonify({"error": "Country code not found"}), 404
+
+if __name__ == '__main__':
+    app.run(port=5000)
